34ea23e5857d776e936b41d01acede2ad9387017
Milan Kovacs <kovacsmilan85@gmail.com>
2017-04-06 12:22:47 +0200

    print todo app usage when no arguments are given

--- Project/src/App.java
+++ Project/src/App.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+
 public class App {
   public static void main(String[] args) {
+    if (args.length == 0) {
+      printUsage();
+    }
+  }
 
+  private static void printUsage() {
+    System.out.println("Java Todo application");
+    System.out.println("=====================");
+    System.out.println();
+    System.out.println("Command line arguments:");
+    System.out.println(" -l   Lists all the tasks");
+    System.out.println(" -a   Adds a new task");
+    System.out.println(" -r   Removes an task");
+    System.out.println(" -c   Completes an task");
   }
 }
